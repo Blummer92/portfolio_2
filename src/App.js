c27cc6a5a045a9398bc5a64e9d8869d65ca56426
Blummer92 <zblumstein@gmail.com>
2019-08-17 17:20:17 -0400

    added my github name

--- src/App.js
+++ src/App.js
@@ -7,10 +7,36 @@ import "./App.css";
 import HeaderNavigation from "../src/HeaderNavigation";
 import Form from "../src/Form";
 import Body from "../src/Body";
+import Popup from '../components/Popup';  
+
+class App extends Component {  
+
+  constructor(props){  
+super(props);  
+this.state = { showPopup: false };  
+}  
+
+  togglePopup() {  
+this.setState({  
+     showPopup: !this.state.showPopup  
+});  
+ }  
+
+
 const App = () => (
   <div>
+    <div>  
+            {this.state.showPopup ?  
+<Popup  
+          text='Click "Close Button" to hide popup'  
+          closePopup={this.togglePopup.bind(this)}  
+/>  
+: null  
+}  
+</div>  
     <HeaderNavigation />
     <Jumbotron fluid>
+    <button onClick={this.togglePopup.bind(this)}> Signnup</button>  
       <h1 className="header">Welcome To React-Bootstrap</h1>
     </Jumbotron>
     <Body>
